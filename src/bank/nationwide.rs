bd24d1be36cb769922341a43f9389e6a33f21d92
John Beisley <johnbeisleyuk@gmail.com>
2019-05-24 09:21:13 +0100

    Improve Debug format for GbpValue.

--- src/bank/nationwide.rs
+++ src/bank/nationwide.rs
@@ -143,14 +143,27 @@ impl<'de> de::Visitor<'de> for InputDateVisitor {
     }
 }
 
-#[derive(Debug)]
 pub struct GbpValue {
     pub pence: i32,
 }
 
+impl GbpValue {
+    pub fn parts(&self) -> (i32, i32) {
+        (self.pence / 100, self.pence % 100)
+    }
+}
+
+impl fmt::Debug for GbpValue {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        let parts = self.parts();
+        write!(f, "GbpValue({}.{})", parts.0, parts.1)
+    }
+}
+
 impl fmt::Display for GbpValue {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        write!(f, "GBP {}.{}", self.pence / 100, self.pence % 100)
+        let parts = self.parts();
+        write!(f, "GBP {}.{}", parts.0, parts.1)
     }
 }
 
