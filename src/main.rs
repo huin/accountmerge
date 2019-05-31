90645324ce8e5fb14cd21fca453498f24ea93694
John Beisley <johnbeisleyuk@gmail.com>
2019-05-31 06:48:06 +0100

    Apply rules to create output transactions and postings.

--- src/main.rs
+++ src/main.rs
@@ -37,13 +37,10 @@ fn main() -> Result<(), Error> {
     let rules = rule::Table::from_path(&opt.rules)?;
     println!("{:?}", rules);
     let input_transactions = bank::nationwide::transactions_from_path(&opt.input)?;
-    for record in &input_transactions {
-        println!("{:?}", record);
-        let cmp = rules.derive_components(record)?;
-        println!("{:?}", cmp);
-    }
-    let output_transactions_result: Result<Vec<_>, Error> =
-        input_transactions.into_iter().map(to_output).collect();
+    let output_transactions_result: Result<Vec<_>, Error> = input_transactions
+        .iter()
+        .map(|in_trn| to_output(&rules, in_trn))
+        .collect();
     let output_transactions = output_transactions_result?;
     for trn in &output_transactions {
         println!("{}", trn);
@@ -53,27 +50,32 @@ fn main() -> Result<(), Error> {
 
 // temporary function to test output format. It's not (entirely) useful until we're labelling
 // accounts properly.
-fn to_output(in_trn: bank::InputTransaction) -> Result<output::Transaction, Error> {
+fn to_output(
+    rules: &rule::Table,
+    in_trn: &bank::InputTransaction,
+) -> Result<output::Transaction, Error> {
     use bank::Paid;
     use output::*;
+    let cmp = rules.derive_components(&in_trn)?;
     Ok(Transaction {
         date: in_trn.date,
         description: format!("{} - {}", in_trn.type_, in_trn.description),
         postings: vec![
             Posting {
-                account: format!(
-                    "assets::account::{}",
-                    in_trn.account_name
-                ),
+                account: cmp
+                    .source_account
+                    .unwrap_or_else(|| "assets::unknown".to_string()),
                 amount: in_trn.paid.src_acct_amt()?,
                 balance: Some(in_trn.balance),
             },
             Posting {
-                account: if let Paid::In(_) = in_trn.paid {
-                    "income::unknown".to_string()
-                } else {
-                    "expenses::unknown".to_string()
-                },
+                account: cmp.dest_account.unwrap_or_else(|| {
+                    if let Paid::In(_) = in_trn.paid {
+                        "income::unknown".to_string()
+                    } else {
+                        "expenses::unknown".to_string()
+                    }
+                }),
                 amount: in_trn.paid.dest_acct_amt()?,
                 balance: None,
             },
