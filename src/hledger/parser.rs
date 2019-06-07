144c32cdb4037446f0c88386bf6a43f2bcc45325
John Beisley <johnbeisleyuk@gmail.com>
2019-06-07 20:53:25 +0100

    Parse transaction code and description.

--- src/hledger/parser.rs
+++ src/hledger/parser.rs
@@ -1,35 +1,13 @@
-/*
-Transactions Transactions are movements of some quantity of commodities between
-named accounts. Each transaction is represented by a journal entry beginning
-with a simple date in column 0. This can be followed by any of the following,
-separated by spaces:
-
-(optional) a status character (empty, !, or *)
-(optional) a transaction code (any short number or text, enclosed in parentheses)
-(optional) a transaction description (any remaining text until end of line or a semicolon)
-(optional) a transaction comment (any remaining text following a semicolon until end of line)
-Then comes zero or more (but usually at least 2) indented lines representing…
-
-Postings A posting is an addition of some amount to, or removal of some amount
-from, an account. Each posting line begins with at least one space or tab (2 or
-4 spaces is common), followed by:
-
-(optional) a status character (empty, !, or *), followed by a space (required)
-an account name (any text, optionally containing single spaces, until end of
-line or a double space) (optional) two or more spaces or tabs followed by an
-amount.
-*/
-
 use std::fmt;
 use std::str::FromStr;
 
 use chrono::NaiveDate;
 use nom::branch::alt;
-use nom::bytes::complete::tag;
+use nom::bytes::complete::{tag, take_while1};
 use nom::character::complete::space1;
-use nom::combinator::{map, map_opt, map_res};
-use nom::sequence::tuple;
-use nom::IResult;
+use nom::combinator::{map, map_opt, map_res, opt};
+use nom::sequence::{pair, preceded, terminated, tuple};
+use nom::{AsChar, IResult, InputTakeAtPosition};
 
 fn date(i: &str) -> IResult<&str, NaiveDate> {
     use num::*;
@@ -50,6 +28,26 @@ fn test_date() {
     assert_eq!(date("2000.1.2"), Ok(("", NaiveDate::from_ymd(2000, 1, 2))));
 }
 
+fn description(i: &str) -> IResult<&str, &str> {
+    map(
+        take_while1(|chr| chr != ';' && chr != '\n' && chr != '\r'),
+        |d: &str| d.trim_end(),
+    )(i)
+}
+
+/// Parses a field parsed by `field`, which must be preceded by one or more
+/// spaces or tabs.
+fn optional_field<I, O, F>(field: F) -> impl Fn(I) -> IResult<I, Option<O>>
+where
+    I: Clone,
+    I: InputTakeAtPosition,
+    F: Fn(I) -> IResult<I, O>,
+    <I as InputTakeAtPosition>::Item: AsChar + Clone,
+{
+    let p = opt(map(pair(space1, field), |(_, v)| v));
+    move |input: I| p(input)
+}
+
 #[derive(Debug, Fail)]
 enum StatusError {
     #[fail(display = "bad status string: {:?}", string)]
@@ -58,7 +56,6 @@ enum StatusError {
 
 #[derive(Debug, Eq, PartialEq)]
 enum Status {
-    Empty,
     Bang,
     Star,
 }
@@ -67,7 +64,6 @@ impl fmt::Display for Status {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         use Status::*;
         match self {
-            Empty => Ok(()),
             Bang => f.write_str("!"),
             Star => f.write_str("*"),
         }
@@ -80,7 +76,6 @@ impl FromStr for Status {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         use Status::*;
         match s {
-            "" => Ok(Empty),
             "!" => Ok(Bang),
             "*" => Ok(Star),
             _ => Err(StatusError::InvalidStatusString { string: s.into() }),
@@ -92,27 +87,52 @@ fn status(i: &str) -> IResult<&str, Status> {
     map_res(alt((tag("!"), tag("*"), tag(""))), Status::from_str)(i)
 }
 
+fn transaction_code(i: &str) -> IResult<&str, &str> {
+    terminated(
+        preceded(
+            tag("("),
+            take_while1(|chr| chr != ')' && chr != '\n' && chr != '\r'),
+        ),
+        tag(")"),
+    )(i)
+}
+
 #[derive(Debug, Eq, PartialEq)]
 struct TransactionHeader {
     date: NaiveDate,
-    status: Status,
+    status: Option<Status>,
+    code: Option<String>,
+    description: Option<String>,
 }
 
 fn transaction_header(i: &str) -> IResult<&str, TransactionHeader> {
-    map(tuple((date, space1, status)), |(date, _, status)| {
-        TransactionHeader { date, status }
-    })(i)
+    map(
+        tuple((
+            date,
+            optional_field(status),
+            optional_field(transaction_code),
+            optional_field(description),
+        )),
+        |(date, status, code, description)| TransactionHeader {
+            date,
+            status,
+            code: code.map(Into::into),
+            description: description.map(Into::into),
+        },
+    )(i)
 }
 
 #[test]
 fn test_transaction_header() {
     assert_eq!(
-        transaction_header("2000/1/2 * TODO"),
+        transaction_header("2000/1/2 * (code) description"),
         Ok((
-            " TODO",
+            "",
             TransactionHeader {
                 date: NaiveDate::from_ymd(2000, 1, 2),
-                status: Status::Star,
+                status: Some(Status::Star),
+                code: Some("code".to_string()),
+                description: Some("description".to_string()),
             }
         ))
     );
