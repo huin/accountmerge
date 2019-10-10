6eab201b9dab16b093da0ddc20383c0015b41597
John Beisley <johnbeisleyuk@gmail.com>
2019-10-10 18:11:58 +0100

    Create script to run examples.

---
+++ run_examples.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p example_output
+
+for i in 1 2; do
+    cargo run -q -- import examples/statement${i}.csv \
+        > example_output/statement${i}-raw.journal
+    cargo run -q -- apply-rules -r examples/rules.ron \
+        example_output/statement${i}-raw.journal \
+        > example_output/statements${i}-ruled.journal
+done
+
+cargo run -q -- merge \
+    examples/initial.journal \
+    example_output/statements*-ruled.journal \
+    > example_output/merged.journal
