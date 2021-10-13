49d72b6f800b590d2a2d0508061b84a0b1582299
John Beisley <johnbeisleyuk@gmail.com>
2021-10-13 11:03:33 +0100

    Add date field to Rhai transaction type.

--- src/rules/rhai/types.rs
+++ src/rules/rhai/types.rs
@@ -2,6 +2,8 @@ use std::collections::{HashMap, HashSet};
 use std::convert::{TryFrom, TryInto};
 
 use anyhow::{Error, Result};
+use chrono::Datelike;
+use rhai::{Dynamic, Engine};
 
 use crate::comment::Comment;
 use crate::internal::{TransactionInternal, TransactionPostings};
@@ -26,7 +28,10 @@ impl From<TransactionPostings> for Map {
         let mut map = rhai::Map::new();
         let comment_map: Map = trn_posts.trn.comment.into();
         map.insert("comment".into(), comment_map.0.into());
-        // pub date: NaiveDate,
+        map.insert(
+            "date".into(),
+            Dynamic::from(NaiveDate(trn_posts.trn.raw.date)),
+        );
         // pub effective_date: Option<NaiveDate>,
         // pub status: Option<TransactionStatus>,
         // pub code: Option<String>,
@@ -40,11 +45,12 @@ impl TryFrom<Map> for TransactionPostings {
     type Error = Error;
     fn try_from(mut map: Map) -> Result<Self> {
         // TODO: Remaining fields.
+        let date: NaiveDate = map.take_value("date")?;
         Ok(TransactionPostings {
             trn: TransactionInternal {
                 raw: ledger_parser::Transaction {
                     comment: None,
-                    date: chrono::NaiveDate::from_ymd(2000, 1, 1),
+                    date: date.0,
                     effective_date: None,
                     status: None,
                     code: None,
@@ -103,5 +109,43 @@ impl TryFrom<Map> for Comment {
                 .collect::<Result<HashMap<String, String>>>()?,
         };
         Ok(comment)
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct NaiveDate(pub chrono::NaiveDate);
+
+impl NaiveDate {
+    pub fn register_type(engine: &mut Engine) {
+        engine
+            .register_type::<Self>()
+            .register_fn("new_date", Self::new)
+            .register_fn("to_debug", |x: &mut Self| format!("{:?}", x))
+            .register_get_set("year", Self::get_year, Self::set_year)
+            .register_get_set("month", Self::get_month, Self::set_month)
+            .register_get_set("day", Self::get_day, Self::set_day);
+    }
+
+    fn new(year: i32, month: u32, day: u32) -> Self {
+        Self(chrono::NaiveDate::from_ymd(year, month, day))
+    }
+
+    fn get_year(&mut self) -> i64 {
+        self.0.year() as i64
+    }
+    fn get_month(&mut self) -> i64 {
+        self.0.month() as i64
+    }
+    fn get_day(&mut self) -> i64 {
+        self.0.day() as i64
+    }
+    fn set_year(&mut self, year: i64) {
+        self.0 = chrono::NaiveDate::from_ymd(year as i32, self.0.month(), self.0.day())
+    }
+    fn set_month(&mut self, month: i64) {
+        self.0 = chrono::NaiveDate::from_ymd(self.0.year(), month as u32, self.0.day())
+    }
+    fn set_day(&mut self, day: i64) {
+        self.0 = chrono::NaiveDate::from_ymd(self.0.year(), self.0.month(), day as u32)
     }
 }
