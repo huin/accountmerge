1acf3676a8f6f6546135d59271e188d65266e9e9
John Beisley <johnbeisleyuk@gmail.com>
2022-11-26 09:45:24 +0000

    Intersperse blank lines in ledger output.

--- src/ledgerutil.rs
+++ src/ledgerutil.rs
@@ -13,9 +13,10 @@ pub fn simple_posting_amount(amount: Amount) -> PostingAmount {
 
 pub fn ledger_from_transactions(transactions: Vec<Transaction>) -> Ledger {
     Ledger {
-        items: transactions
-            .into_iter()
-            .map(LedgerItem::Transaction)
-            .collect(),
+        items: itertools::intersperse(
+            transactions.into_iter().map(LedgerItem::Transaction),
+            LedgerItem::EmptyLine,
+        )
+        .collect(),
     }
 }
